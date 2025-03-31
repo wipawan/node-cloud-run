1baad1bda2ade81574c4617a5909b7ddc75721f2
mew <mew.amornchairattanakul@datadoghq.com>
2025-04-01 01:27:32 +0800

    fix cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const logger = createLogger({
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, traceparent");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
